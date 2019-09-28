ac099fadc91241bcd1538d60501aec288f056e7d
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-09-28 13:44:15 -0400

    #1 Create the project

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>TOP</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
